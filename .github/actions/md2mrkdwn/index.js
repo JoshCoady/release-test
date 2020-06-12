d3816de51fcb519587e5e3d0ab11fbc4843f22d6
Joshua Coady <josh@lovetoknow.com>
2020-06-12 10:15:43 -0700

    feat: switch to using published slackify action

--- .github/actions/md2mrkdwn/index.js
+++
@@ -1,9 +0,0 @@
-const core = require('@actions/core');
-const slackifyMarkdown = require('slackify-markdown');
-try {
-    const md = core.getInput('text');
-    const mrkdwn = slackifyMarkdown(md);
-    core.setOutput("text", mrkdwn);
-} catch (error) {
-    core.setFailed(error.message);
-}
